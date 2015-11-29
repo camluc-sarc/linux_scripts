c2ff35cb8136f7b7a5665528271eecb83e8af609
camluc <camluc.sarc@gmail.com>
2015-11-29 04:48:08 -0600

    add scripts/install_jenkins.sh

---
+++ scripts/install_jenkins.sh
@@ -0,0 +1,15 @@
+# install jdk
+#
+echo "Installing JDK..."
+sudo apt-get install default-jdk -y
+
+
+# install Jenkins
+#
+echo "Installing Jenkins..."
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
+# Jenkins started on port 8080
+# if /etc/init.d/jenkins fails to start, edit /etc/default/jenkins to change port
